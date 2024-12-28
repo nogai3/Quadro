b03b29616b65eef48df448211bafee21c74203bd
nogai3 <thebrianmaps142@gmail.com>
2024-12-29 00:07:49 +0500

    asd

--- script.js
+++ script.js
@@ -21,29 +21,29 @@ function Clicked() {
         x1 = BInput * (-1) - dis1;
         x1 = x1 / 2 * AInput;
         X1Output.textContent = `x1 = -b - √D / 2 * a = -${BInput} - ${dis1} / 2 * ${AInput} = ${x1}`;
-        console.log(X1Output);
+        console.log(X1Output.textContent);
         x2 = BInput * (-1) + dis1;
         x2 = x2 / 2 * AInput;
         X2Output.textContent = `x2 = -b + √D / 2 * a = -${BInput} + ${dis1} / 2 * ${AInput} = ${x2}`;
-        console.log(X2Output);
+        console.log(X2Output.textContent);
         Answer.textContent = `Ответ: x1 = ${x1}, x2 = ${x2}`;
-        console.log(Answer);
+        console.log(Answer.textContent);
     }
     else if (dis <= 0) {
         X1Output.textContent = 'Нет решения! D = 0';
-        console.log(X1Output);
+        console.log(X1Output.textContent);
         Answer.textContent = '∅';
-        console.log(Answer);
+        console.log(Answer.textContent);
     }
     else {
         X1Output.textContent = 'D = 0. Нахожу только x1...'
-        console.log(X1Output);
+        console.log(X1Output.textContent);
         dis1 = Math.sqrt(dis);
         x1 = BInput * (-1) - dis1;
         x1 = x1 / 2 * AInput;
         X2Output.textContent = `x1 = -b - √D / 2 * a = -${BInput} - ${dis1} / 2 * ${AInput} = ${x1}`;
-        console.log(X2Output);
+        console.log(X2Output.textContent);
         Answer.textContent = `Ответ: x1 = ${x1} (т.к D = 0)`;
-        console.log(Answer);
+        console.log(Answer.textContent);
     }
 }
